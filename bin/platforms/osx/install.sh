0806aab5d0fda7a53e1c77e48ff44757456d7307
yukihira1992 <hirayama@cancerscan.jp>
2018-04-04 15:27:14 +0900

    Implement dotfiles.

---
+++ bin/platforms/osx/install.sh
@@ -0,0 +1,32 @@
+#! /bin/sh
+#
+# install.sh
+#
+# Install minimal packages by homebrew.
+#
+# Copyright (C) 2018 yukihira1992 <hirayama@cancerscan.jp>
+#
+# Distributed under terms of the MIT license.
+#
+
+echo "Install to OSX."
+
+# install Xcode command line tools
+xcode-select --install
+
+# install Homebrew
+echo "Install homebrew."
+if !(type brew > /dev/null 2>&1); then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew update
+
+# install brew-file
+brew install rcmdnk/file/brew-file
+
+# install vim
+brew install vim --enable-interp=python,python3,ruby
+
+# install packages by brew-file
+brew file install
