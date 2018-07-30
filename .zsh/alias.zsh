6f0bc7ddb460c30df88bd905850c7ec9861ebcc4
kitagawa-hr <kitagawa@cancerscan.jp>
2018-07-30 09:53:49 +0900

    add some alias

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -1,19 +1,25 @@
+# application
 alias jb='jupyter notebook'
 alias jl='jupyter lab'
 alias sp3='~/.pyenv/shims/spyder3'
 alias vi='nvim'
+alias oni='/Applications/Oni.app/Contents/MacOS/Oni'
+
+# commands 
 alias la='ls -a'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 alias rm='rm -i'
 alias mv='mv -i'
-alias oni='/Applications/Oni.app/Contents/MacOS/Oni'
 alias sed='gsed'
+
 # git
 alias gs='git status'
 alias gss='git status --short --branch'
 alias gr="git log --graph --date=short --decorate=short --pretty=format:'%Cgreen%h %Creset%cd %Cblue%cn %Cred%d %Creset%s'"
-alias gc='git checkout'
+alias g='cd $(ghq root)/$(ghq list | peco)'
+alias gh='hub browse $(ghq list | peco | cut -d "/" -f 2,3)'
+alias gch='git checkout `git branch | peco | sed -e "s/\* //g" | awk "{print \$1}"`'
 alias gb='git branch'
 alias cm='git commit -m'
 alias push='git push'
@@ -21,5 +27,3 @@ alias add='git add'
 alias pull='git pull'
 alias commit='git commit'
 eval $(thefuck --alias)
-alias g='cd $(ghq root)/$(ghq list | peco)'
-alias gh='hub browse $(ghq list | peco | cut -d "/" -f 2,3)'
