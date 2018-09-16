f3bc024597ad5b677b70b3862e2a111a5b251d55
kitagawa-hr <kitagawa@cancerscan.jp>
2018-09-16 18:03:55 +0900

    modify linuxrc and add xkeysnail config

--- xkeysnail/config.py
+++ xkeysnail/config.py
@@ -1,18 +1,29 @@
 # -*- coding: utf-8 -*-
 
 import re
-from xkeysnail.transform import *
+from xkeysnail.transform import define_multipurpose_modmap, define_keymap, Key, K
+
 
 define_multipurpose_modmap(
-    # Capslock is escape when pressed and released. Control when held down.
     {
-        Key.CAPSLOCK: [Key.ESC, Key.LEFT_CTRL]
-    }, "CapsLock as ESC when pressed, Ctrl when held"
+    Key.CAPSLOCK: [Key.ESC, Key.LEFT_CTRL],
+    Key.MUHENKAN: [Key.MUHENKAN, Key.LEFT_ALT],
+    Key.HENKAN: [Key.HENKAN, Key.RIGHT_CTRL]
+    }
 
+)
+
+# define_keymap(condition, mapping, name)
+
+define_keymap(
+    None,
+    {
+        K('esc'): [K('muhenkan'), K('esc')]
+    },"IME off when Escaped"
 )
 
 define_keymap(
-    re.compile('Tilix'), {
-        K('esc'): [K('muhenkan'), K('esc')]
-    }, "Esc and IME off"
-)
+    re.compile("Firefox|Google-chrome"), {
+    K("K"): K("C-TAB"),
+    K("J"): K("C-Shift-TAB"),
+}, "Vim on Chrome")
