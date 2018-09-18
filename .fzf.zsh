f17d829af8bd88eac7e7449c5e278c5527d66786
kitagawa-hr <kitagawa@cancerscan.jp>
2018-09-18 22:40:13 +0900

    add fzf docker completion

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,19 +1,80 @@
 # Setup fzf
-# ---------
-if [[ ! "$PATH" == *${HOME}/.cache/dein/repos/github.com/junegunn/fzf/bin* ]]; then
-  export PATH="$PATH:${HOME}/.cache/dein/repos/github.com/junegunn/fzf/bin"
+# setting
+
+export FZF_REPO="${HOME}/.zplug/repos/junegunn/fzf"
+if [[ ! "$PATH" == *${FZF_REPO}/bin* ]]; then
+  export PATH="$PATH:${FZF_REPO}/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "${HOME}/.cache/dein/repos/github.com/junegunn/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "${FZF_REPO}/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "${HOME}/.cache/dein/repos/github.com/junegunn/fzf/shell/key-bindings.zsh"
+source "${FZF_REPO}/shell/key-bindings.zsh"
 
-# setting
 export FZF_DEFAULT_COMMAND='rg --files --hidden --glob "!.git"'
 export FZF_DEFAULT_OPTS='--height 40% --reverse --border'
 export FZF_COMPLETION_TRIGGER=','
 
+# Custom fuzzy completion for "docker" command
+#   e.g. docker **<TAB>
+export DOCKER_FZF_PREFIX="--bind ctrl-a:select-all,ctrl-d:deselect-all,ctrl-t:toggle-all"
+
+_fzf_complete_docker_run_post() {
+    awk '{print $1":"$2}'
+}
+
+_fzf_complete_docker_run () {
+    _fzf_complete "$DOCKER_FZF_PREFIX -m --header-lines=1" "$@" < <(
+        docker images
+    )
+}
+
+_fzf_complete_docker_common_post() {
+    awk -F"\t" '{print $1}'
+}
+
+_fzf_complete_docker_common () {
+    _fzf_complete "$DOCKER_FZF_PREFIX --reverse -m" "$@" < <(
+        docker images --format "{{.Repository}}:{{.Tag}}\t {{.ID}}"
+    )
+}
+
+_fzf_complete_docker_container_post() {
+    awk '{print $NF}'
+}
+
+_fzf_complete_docker_container () {
+    _fzf_complete "$DOCKER_FZF_PREFIX -m --header-lines=1" "$@" < <(
+        docker ps -a
+    )
+}
+
+
+_fzf_complete_docker() {
+    local tokens docker_command
+    setopt localoptions noshwordsplit noksh_arrays noposixbuiltins
+    # http://zsh.sourceforge.net/FAQ/zshfaq03.html
+    # http://zsh.sourceforge.net/Doc/Release/Expansion.html#Parameter-Expansion-Flags
+    tokens=(${(z)LBUFFER})
+    if [ ${#tokens} -le 2 ]; then
+        return
+    fi
+    docker_command=${tokens[2]}
+    case "$docker_command" in
+        run)
+            _fzf_complete_docker_run "$@"
+            return
+        ;;
+        exec|rm)
+            _fzf_complete_docker_container "$@"
+            return
+        ;;
+        save|load|push|pull|tag|rmi)
+            _fzf_complete_docker_common "$@"
+            return
+        ;;
+    esac
+  }
