6ec910896c78673cf3a7c5637245e36166e14ff6
kitagawa-hr <kitagawa@cancerscan.jp>
2018-10-28 23:35:13 +0900

    add runghc func

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -7,3 +7,9 @@ tex(){
   docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja uplatex $file && \
   docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja dvipdfmx ${file/tex/dvi}
 }
+
+
+runghc(){
+  local file=/workdir/$1
+  docker run --rm -v $PWD:/workdir haskell runghc $file
+}
