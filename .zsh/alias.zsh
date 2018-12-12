9f26365faa50a9da4c0d50658afc62f3503ed10b
kitagawa-hr <kitagawa@cancerscan.jp>
2018-12-13 00:37:36 +0900

    Update alias.zsh

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -1,16 +1,23 @@
-# application
+# ------------------------------------
+# Applications
+# ------------------------------------
 alias jb='jupyter notebook'
 alias jl='jupyter lab'
 alias v='nvim'
+alias zp='hub browse $(zplug list | fzf)'
 
-# commands 
+# ------------------------------------
+# Shell Commands
+# ------------------------------------
 alias la='ls -a'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 alias rm='rm -i'
 alias mv='mv -i'
 
-# git
+# ------------------------------------
+# Git Commands
+# ------------------------------------
 alias gs='git status'
 alias gss='git status --short --branch'
 alias gr="git log --graph --date=short --decorate=short --pretty=format:'%Cgreen%h %Creset%cd %Cblue%cn %Cred%d %Creset%s'"
@@ -24,7 +31,45 @@ alias push='git push origin HEAD'
 alias pull='git pull origin $(git rev-parse --abbrev-ref HEAD)'
 alias add='git add'
 alias commit='git commit'
-eval $(thefuck --alias)
 
-# zplug
-alias zp='hub browse $(zplug list | fzf)'
+# ------------------------------------
+# Docker commands
+# ------------------------------------
+
+# Get latest container ID
+alias dl="docker ps -l -q"
+
+# Get container process
+alias dps="docker ps"
+
+# Get process included stop container
+alias dpa="docker ps -a"
+
+# Get images
+alias di="docker images"
+
+# Get container IP
+alias dip="docker inspect --format '{{ .NetworkSettings.IPAddress }}'"
+
+# Run deamonized container, e.g., $dkd base /bin/echo hello
+alias dkd="docker run -d -P"
+
+# Run interactive container, e.g., $dki base /bin/bash
+alias dki="docker run -i -t -P"
+
+# Execute interactive container, e.g., $dex base /bin/bash
+alias dex="docker exec -i -t"
+
+# Stop all containers
+dstop() { docker stop $(docker ps -a -q); }
+
+# Remove all containers
+drm() { docker rm $(docker ps -a -q); }
+
+# Stop and Remove all containers
+alias drmf='docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'
+
+# Remove all images
+dri() { docker rmi $(docker images -q); }
+
+
