3ab0e0e8c2898899307207094cac5d9d17476c42
Haruki Kitagawa <kitagawahr@gmail.com>
2021-03-15 23:35:12 +0900

    Add zoxide and mcfly

--- dot_zsh/keybind.zsh
+++ dot_zsh/keybind.zsh
@@ -43,29 +43,6 @@ bindkey -M viins '^]' insert-last-word
 #
 # functions
 #
-_fzf-select-history() {
-    if true; then
-        BUFFER="$(
-        history 1 \
-            | sort -k1,1nr \
-            | perl -ne 'BEGIN { my @lines = (); } s/^\s*\d+\s*//; $in=$_; if (!(grep {$in eq $_} @lines)) { push(@lines, $in); print $in; }' \
-            | fzf --query "$LBUFFER"
-        )"
-
-        CURSOR=$#BUFFER
-        #zle accept-line
-        #zle clear-screen
-        zle reset-prompt
-    else
-        if is-at-least 4.3.9; then
-            zle -la history-incremental-pattern-search-backward && bindkey "^r" history-incremental-pattern-search-backward
-        else
-            history-incremental-search-backward
-        fi
-    fi
-}
-zle -N _fzf-select-history
-bindkey '^r' _fzf-select-history
 
 function is_git_repo()
 {
--- dot_zsh/zinit.zsh
+++ dot_zsh/zinit.zsh
@@ -14,3 +14,13 @@ zinit wait lucid light-mode for \
   atinit"ZINIT[COMPINIT_OPTS]=-C; zicompinit; zicdreplay" zdharma/fast-syntax-highlighting \
   blockf zsh-users/zsh-completions \
   atload"!_zsh_autosuggest_start" zsh-users/zsh-autosuggestions
+
+export ZSH_AUTOSUGGEST_USE_ASYNC=1
+
+zinit lucid from'gh-r' as'program' for \
+  mv'zoxide* -> zoxide' atload'eval "$(zoxide init zsh)"' ajeetdsouza/zoxide \
+  mv'mcfly* -> mcfly' atload'eval "$(mcfly init zsh)"' cantino/mcfly
+
+export MCFLY_KEY_SCHEME=vim
+export MCFLY_FUZZY=true
+
