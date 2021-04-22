e153a3b295d5a4a35d1f60f7c01593f97e19c731
kitagawa-hr <kitagawa@cancerscan.jp>
2021-04-22 13:45:51 +0900

    Bundle separated files to .zshrc

--- dot_zsh/keybind.zsh
+++
@@ -1,5 +0,0 @@
-# Insert a last word
-autoload -Uz smart-insert-last-word
-zle -N insert-last-word smart-insert-last-word
-zstyle :insert-last-word match '*([^[:space:]][[:alpha:]/\\]|[[:alpha:]/\\][^[:space:]])*'
-bindkey -M viins '^]' insert-last-word
--- dot_zsh/prompt.zsh
+++
@@ -1,6 +0,0 @@
-# install starship if not installed
-if ! type starship > /dev/null; then
-    command curl -fsSL https://starship.rs/install.sh | bash
-fi
-
-eval "$(starship init zsh)"
