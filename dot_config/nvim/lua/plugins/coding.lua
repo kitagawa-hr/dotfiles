1e6e667c5e8007495514e352e16cf070608b1220
Haruki Kitagawa <kitagawahr@gmail.com>
2025-04-30 16:24:59 +0900

    Add neotest

--- dot_config/nvim/lua/plugins/coding.lua
+++ dot_config/nvim/lua/plugins/coding.lua
@@ -267,6 +267,38 @@ return {
   },
   { "wakatime/vim-wakatime", lazy = false },
   {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "rouge8/neotest-rust",
+      "lawrence-laz/neotest-zig",
+    },
+    opts = function()
+      return {
+        adapters = {
+          require("neotest-rust"),
+          require("neotest-zig")({ dap = { adapter = "codelldb" } }),
+        },
+      }
+    end,
+    -- stylua: ignore
+    keys = {
+      { "<leader>tt", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run File" },
+      { "<leader>tT", function() require("neotest").run.run(vim.uv.cwd()) end, desc = "Run All Test Files" },
+      { "<leader>tr", function() require("neotest").run.run() end, desc = "Run Nearest" },
+		---@diagnostic disable-next-line: missing-fields
+      { "<leader>td", function() require("neotest").run.run({ strategy = "dap"}) end, desc = "Debug Nearest" },
+      { "<leader>tl", function() require("neotest").run.run_last() end, desc = "Run Last" },
+      { "<leader>ts", function() require("neotest").summary.toggle() end, desc = "Toggle Summary" },
+      { "<leader>to", function() require("neotest").output.open({ enter = true, auto_close = true }) end, desc = "Show Output" },
+      { "<leader>tO", function() require("neotest").output_panel.toggle() end, desc = "Toggle Output Panel" },
+      { "<leader>tS", function() require("neotest").run.stop() end, desc = "Stop" },
+    },
+  },
+  {
     "folke/snacks.nvim",
     priority = 1000,
     lazy = false,
